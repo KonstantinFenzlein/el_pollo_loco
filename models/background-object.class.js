5aa3baa7eaf259d8bf333b730560773567ec0501
KonstantinFenzlein <kfenzlein@googlemail.com>
2026-08-16 22:32:13 +0200

    update pics + background.class.js + chicks.html

---
+++ models/background-object.class.js
@@ -0,0 +1,14 @@
+/**
+* class BackgroundObject
+* extends MovableObject
+*/
+class BackgroundObject extends MovableObject {
+  width = 720;
+  height = 480;
+
+  constructor(imagePath, x) {
+    super().loadImg(imagePath);
+    this.x = x;
+    this.y = 480 - this.height;
+  }
+}
